dfc8f2383696733800beb6c6f604e3098d613921
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-19 17:42:25 -0400

    [module] Add setup.py
    setup.py will allow the package installation via pip command.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="ibmcloud_python_sdk",
+      version="1.0.0",
+      description="IBM Cloud Python SDK",
+      url="#",
+      author="IBM Corp",
+      author_email="#",
+      license="MIT",
+      packages=["ibmcloud_python_sdk"],
+      zip_safe=False)
