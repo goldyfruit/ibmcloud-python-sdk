e87f0ac817f6dfbb5392baa0410125bb6541be81
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-19 16:33:18 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,28 @@
+import http.client
+import json
+import os
+from token import getToken
+
+
+MANDATORY_ENV_VARS = ["IC_VERSION", "IC_API_KEY", "IC_REGION", "IC_GENERATION"]
+
+for var in MANDATORY_ENV_VARS:
+    if var not in os.environ:
+        raise EnvironmentError(f"Failed because {var} is not set.")
+
+version = os.environ.get("IC_VERSION")
+region = os.environ.get("IC_REGION")
+key = os.environ.get("IC_API_KEY")
+generation = os.environ.get("IC_GENERATION")
+authUrl = "iam.cloud.ibm.com"
+url = f"{region}.iaas.cloud.ibm.com"
+
+conn = http.client.HTTPSConnection(url)
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': getToken(authUrl, key),
+}
+
+
---
+++ vpc.py
@@ -0,0 +1,50 @@
+import http.client
+import json
+from config import conn, headers, version, generation
+from common import printJson
+
+
+# Get all VPC
+# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/list_vpcs
+# Doc: https://cloud.ibm.com/apidocs/vpc#list-all-vpcs
+def getVpcs():
+
+    try:
+        # Connect to api endpoint for vpcs
+        path = f"/v1/vpcs?version={version}&generation={generation}"
+        conn.request("GET", path, None, headers)
+
+        # Get and read response data
+        res = conn.getresponse()
+        data = res.read()
+
+        # Print and return response data
+        printJson(data.decode("utf-8"))
+        return data.decode("utf-8")
+
+    except Exception as error:
+        print(f"Error fetching VPCs. {error}")
+        raise
+
+
+# Get specific VPC
+# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc
+# Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-specified-vpc
+def getVpc(id):
+
+    try:
+        # Connect to api endpoint for vpcs
+        path = f"/v1/vpcs/{id}?version={version}&generation={generation}"
+        conn.request("GET", path, None, headers)
+
+        # Get and read response data
+        res = conn.getresponse()
+        data = res.read()
+
+        # Print and return response data
+        printJson(data.decode("utf-8"))
+        return data.decode("utf-8")
+
+    except Exception as error:
+        print(f"Error fetching VPC. {error}")
+        raise
