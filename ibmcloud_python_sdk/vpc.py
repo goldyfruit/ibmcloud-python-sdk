e3a79e750f4f848cb97dac5ef70b3b202285add5
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-19 19:17:03 -0400

    [vpc] Retrieve default network ACL
    This function retrieves VPC's default network ACL

--- ibmcloud_python_sdk/vpc.py
+++ ibmcloud_python_sdk/vpc.py
@@ -7,7 +7,6 @@ from .config import conn, headers, version, generation
 # Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/list_vpcs
 # Doc: https://cloud.ibm.com/apidocs/vpc#list-all-vpcs
 def get_vpcs():
-
     try:
         # Connect to api endpoint for vpcs
         path = f"/v1/vpcs?version={version}&generation={generation}"
@@ -29,7 +28,6 @@ def get_vpcs():
 # Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc
 # Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-specified-vpc
 def get_vpc(id):
-
     try:
         # Connect to api endpoint for vpcs
         path = f"/v1/vpcs/{id}?version={version}&generation={generation}"
@@ -44,4 +42,25 @@ def get_vpc(id):
 
     except Exception as error:
         print(f"Error fetching VPC. {error}")
+        raise
+
+
+# Get VPC default network ACL
+# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc_default_network_acl
+# Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-a-vpc-s-default-network-acl
+def get_vpc_default_network_acl(id):
+    try:
+        # Connect to api endpoint for vpcs
+        path = f"/v1/vpcs/{id}/default_network_acl?version={version}&generation={generation}"
+        conn.request("GET", path, None, headers)
+
+        # Get and read response data
+        res = conn.getresponse()
+        data = res.read()
+
+        # Print and return response data
+        return json.loads(data)
+
+    except Exception as error:
+        print(f"Error fetching VPC default network ACL. {error}")
         raise
