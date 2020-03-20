64339d2cf519a379ba5f7743f394f5074c11beb4
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-20 17:21:44 -0400

    [examples] Update the examples
    Few examples have been added and changes have been
    applied to reflect the class change.

--- examples/vpc.py
+++ examples/vpc.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from ibmcloud_python_sdk import vpc
+from ibmcloud_python_sdk import vpc as ic
 
+# Intentiate the class
+vpc = ic.Vpc()
 
 # Retrieve a complete VPC list
 vpc.get_vpcs()
 
-# Retrieve specific VPC
-vpc.get_vpc("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
+# Retrieve specific VPC by ID
+vpc.get_vpc_by_id("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
+
+# Retrieve specific VPC by name
+vpc.get_vpc_by_name("advisory")
