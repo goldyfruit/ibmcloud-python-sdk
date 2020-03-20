9cae9dcfc4cab509b457a358ba1eb61676243aac
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-20 17:45:06 -0400

    [examples] Add VPC create example

--- examples/vpc.py
+++ examples/vpc.py
@@ -13,3 +13,6 @@ vpc.get_vpc_by_id("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
 
 # Retrieve specific VPC by name
 vpc.get_vpc_by_name("advisory")
+
+# Create VPC
+vpc.create_vpc(name="my-first-vpc", resource_group="f328f2cdec6d4b4da2844c214dec9d39")
