d1af3948a42cb388bee75c707649a0bd9a553825
James Regis <james.regis@bara.ca>
2020-03-20 16:37:41 -0400

    Changes in instance.py and vpc.py

--- ibmcloud_python_sdk/instance.py
+++ ibmcloud_python_sdk/instance.py
@@ -4,11 +4,11 @@ from .config import conn, headers, version, generation
 
 
 # Get all VPC
-# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/list_vpcs
-# Doc: https://cloud.ibm.com/apidocs/vpc#list-all-vpcs
+# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/list_instances
+# Doc: https://cloud.ibm.com/apidocs/instance#list-all-instances
 def get_instances():
     try:
-        # Connect to api endpoint for vpcs
+        # Connect to api endpoint for instances
         path = f"/v1/instances?version={version}&generation={generation}"
         conn.request("GET", path, None, headers)
 
@@ -25,12 +25,12 @@ def get_instances():
 
 
 # Get specific VPC
-# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc
-# Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-specified-vpc
-def get_instance(id):
+# Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_instance
+# Doc: https://cloud.ibm.com/apidocs/instance#retrieve-specified-instance
+def get_instance_by_id(id):
     try:
-        # Connect to api endpoint for vpcs
-        path = f"/v1/instances/{id}?version={version}&generation={generation}"
+        # Connect to api endpoint for instance
+        path = f"/v1/instances?version={version}&generation={generation}"
         conn.request("GET", path, None, headers)
 
         # Get and read response data
@@ -44,10 +44,32 @@ def get_instance(id):
         print(f"Error fetching instance with id {id}. {error}")
         raise
 
+# Get instance by name
+def get_instance_by_name(name):
+    try:
+        # Connect to api endpoint for instance
+        path = f"/v1/instances/?version={version}&generation={generation}"
+        conn.request("GET", path, None, headers)
+
+        # Get and read response data
+        res = conn.getresponse()
+        data = res.read()
+
+        for instance in json.loads(data)['instances']: 
+            if instance['name'] == name:
+                # Return response data
+                return(instance)
+        # Print and return response data
+        return {"instance": None} 
+
+    except Exception as error:
+        print(f"Error fetching instances. {error}")
+        raise
+
 # create an instance
 def create_instance():
     try:
-        # Connect to api endpoint for vpcs
+        # Connect to api endpoint for instances
         path = f"/v1/instances?version={version}&generation={generation}"
         body = {}
         conn.request("PUT", path, body, headers)
@@ -55,12 +77,12 @@ def create_instance():
         print(f"Error fetching instances. {error}")
         raise
 ## Get VPC default network ACL
-## Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc_default_network_acl
-## Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-a-vpc-s-default-network-acl
-#def get_vpc_default_network_acl(id):
+## Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_instance_default_network_acl
+## Doc: https://cloud.ibm.com/apidocs/instance#retrieve-a-instance-s-default-network-acl
+#def get_instance_default_network_acl(id):
 #    try:
-#        # Connect to api endpoint for vpcs
-#        path = f"/v1/vpcs/{id}/default_network_acl?version={version}&generation={generation}"
+#        # Connect to api endpoint for instances
+#        path = f"/v1/instances/{id}/default_network_acl?version={version}&generation={generation}"
 #        conn.request("GET", path, None, headers)
 #
 #        # Get and read response data
--- ibmcloud_python_sdk/vpc.py
+++ ibmcloud_python_sdk/vpc.py
@@ -27,7 +27,7 @@ def get_vpcs():
 # Get specific VPC
 # Spec: https://pages.github.ibm.com/riaas/api-spec/spec_aspirational/#/VPCs/get_vpc
 # Doc: https://cloud.ibm.com/apidocs/vpc#retrieve-specified-vpc
-def get_vpc(id):
+def get_vpc_by_id(id):
     try:
         # Connect to api endpoint for vpcs
         path = f"/v1/vpcs/{id}?version={version}&generation={generation}"
@@ -42,6 +42,28 @@ def get_vpc(id):
 
     except Exception as error:
         print(f"Error fetching VPC. {error}")
+        raise
+
+# Get VPC by name
+def get_vpc_by_name(name):
+    try:
+        # Connect to api endpoint for instance
+        path = f"/v1/vpcs/?version={version}&generation={generation}"
+        conn.request("GET", path, None, headers)
+
+        # Get and read response data
+        res = conn.getresponse()
+        data = res.read()
+
+        for vpc in json.loads(data)['vpcs']: 
+            if vpc['name'] == name:
+                # Return response data
+                return(vpc)
+        # Print and return response data
+        return {"vpc": None} 
+
+    except Exception as error:
+        print(f"Error fetching instances. {error}")
         raise
 
 
