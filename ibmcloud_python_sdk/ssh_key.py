f4d21752a1f745a03819592ce7a7cdca05f9e63e
James Regis <james.regis@bara.ca>
2020-03-22 10:54:51 -0400

    [ssh-key] Add Key() class

---
+++ ibmcloud_python_sdk/ssh_key.py
@@ -0,0 +1,114 @@
+import json
+from . import config as ic 
+
+class Key():
+
+    def __init__(self):
+        self.cfg = ic.Config()
+        self.ver = self.cfg.version
+        self.gen = self.cfg.generation
+        self.headers = self.cfg.headers
+        self.conn = self.cfg.conn
+
+    # Get all ssh-keys
+    def get_keys(self):
+
+        try:
+            # Connect to api endpoint for keys
+            path = ("/v1/keys?version={}&generation={}").format(
+                self.ver, self.gen)
+            self.conn.request("GET", path, None, self.headers)
+
+            # Get and read response data
+            res = self.conn.getresponse()
+            data = res.read()
+
+            # Print and return response data
+            return json.loads(data)
+
+        except Exception as error:
+            print(f"Error fetching keys. {error}")
+            raise
+
+
+    # Get specific ssh-key by ID
+    def get_key_by_id(self, id):
+        try:
+            # Connect to api endpoint for keys
+            path = ("/v1/keys/{}?version={}&generation={}").format(
+                id, self.ver, self.gen)
+            self.conn.request("GET", path, None, self.headers)
+
+            # Get and read response data
+            res = self.conn.getresponse()
+            data = res.read()
+
+            # Print and return response data
+            return json.loads(data)
+
+        except Exception as error:
+            print(f"Error fetchingi ssh-key with ID {id}. {error}")
+            raise
+
+    # Get specific ssh-key by name
+    def get_key_by_name(self, name):
+        try:
+            # Connect to api endpoint for keys
+            path = ("/v1/keys/?version={}&generation={}").format(
+                self.ver, self.gen)
+            self.conn.request("GET", path, None, self.headers)
+
+            # Get and read response data
+            res = self.conn.getresponse()
+            data = res.read()
+
+            # Loop over instance until filter match
+            for key in json.loads(data)['keys']:
+                if key['name'] == name:
+                    # Return response data
+                    return key
+
+            # Return response if no ssh-key is found
+            return {"errors": [{"code": "not_found"}]}
+
+        except Exception as error:
+            print(f"Error fetching ssh-key with name {name}. {error}")
+            raise
+
+    # Create ssh-key
+    def create_key(self, **kwargs):
+        # Required parameters
+        required_args = set(["name", "public_key", "resource_group"])
+        if not required_args.issubset(set(kwargs.keys())):
+            raise KeyError(
+                f'Required param is missing. Required: {required_args}'
+            )
+
+        # Set default value is not required paramaters are not defined
+        args = {
+            'name': kwargs.get('name'),
+            'public_key': kwargs.get('public_key'),
+            'resource_group': kwargs.get('resource_group'),
+            'type': kwargs.get('resource_group', 'rsa'),
+        }
+
+        # Construct payload
+        payload = {}
+        for key, value in args.items():
+            payload[key] = value
+        try:
+            # Connect to api endpoint for keys
+            path = ("/v1/keys?version={}&generation={}").format(
+                self.ver, self.gen)
+            self.conn.request("POST", path, json.dumps(payload), self.headers)
+
+            # Get and read response data
+            res = self.conn.getresponse()
+            data = res.read()
+
+            # Print and return response data
+            return json.loads(data)
+
+        except Exception as error:
+            print(f"Error creating ssh-key. {error}")
+            raise
