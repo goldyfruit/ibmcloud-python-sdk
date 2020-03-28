af91c2a92ea9c0460797072b35ba5849a5655aea
James Regis <james.regis@bara.ca>
2020-03-28 19:24:10 -0400

    [tests] add vpc folder

--- tests/vpc/common.py
+++ tests/vpc/common.py
@@ -21,9 +21,9 @@ def fake_get_call(service, verb, path, headers):
     if 'vpcs' in path:
         folder = 'vpc'
         result["data"]["vpcs"] = []
-    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
-    # Must return a file-like object
+    resource_file = os.path.normpath('tests/{}/resources/{}/{}.json').format(folder, folder, folder)
     print(resource_file)
+    # Must return a file-like object
     try:
         json_file = open(resource_file, mode='rb')
         result["data"]["vpcs"].append(json.load(json_file))
@@ -41,7 +41,8 @@ def fake_get_one(service, verb, path, headers):
     
     if 'vpc' in path:
         folder = 'vpc'
-    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
+    resource_file = os.path.normpath('tests/{}/resources/{}/{}.json').format(folder, folder, folder)
+    #resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
     # Must return a file-like object
     try:
         json_file = open(resource_file, mode='rb')
@@ -51,19 +52,4 @@ def fake_get_one(service, verb, path, headers):
         return["data"]["vpcs"].append({"error": "not found"})
         raise
 
-def fake_create_vpc(service, verb, path, headers, payload):
-    """
-    Test
-    """
-    #r = requests.Response()
-    path_pattern = '/v1\/vpcs\?version=[0-9]{4}-[0-9]{2}-[0-9]{2}&generation=[0-9]'
-    contain_path = re.compile(path_pattern)
-    if service == "iaas" and verb == "POST" and bool(contain_path.search(path)) is True:
-        data = {}
-        data = { "id": "r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82", "crn": "crn:v1:bluemix:public:is:us-south:a/2d171b8a90e246fd9ffe0e5e8c191c9e::vpc:r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82", "href": "https://us-south.iaas.cloud.ibm.com/v1/vpcs/r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82", "name": "sdk", "status": "available"}
-        return({"status_code": 200, "data": data})
-    else:
-        data = {}
-        data = {"id": "", "name": "", "status": "error !"}
-        return({"status_code": 500, "data": data})
 
--- tests/vpc/test_vpc.py
+++ tests/vpc/test_vpc.py
@@ -5,13 +5,14 @@ from mock import patch
 import ibmcloud_python_sdk.config
 from ibmcloud_python_sdk.vpc.vpc import Vpc
 
-from . import vpc_test_config as tcfg
+from . import common as common
+from . import fake_vpc_function as vpc
 
 class VPCTestCase(unittest.TestCase):
     """Test case for the client methods."""
 
     def setUp(self):
-        self.patcher = patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_get_call)
+        self.patcher = patch('ibmcloud_python_sdk.vpc.vpc.qw', common.fake_get_call)
         self.patcher.start()
         self.vpc = Vpc()
         self.fake_vpc = {}
@@ -23,53 +24,52 @@ class VPCTestCase(unittest.TestCase):
     def tearDown(self):
         self.patcher.stop()
 
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
     def test_get_vpcs(self):
         """Test get_vpcs ."""
         response = self.vpc.get_vpcs()
-        print("hello")
-        print(response)
         self.assertIn('vpcs', response)
 
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
     def test_get_vpc_with_name(self):
         """Test get_vpc with name as parameter."""
         response = self.vpc.get_vpc(self.fake_vpc['name'])
         self.assertEqual(response['name'], self.fake_vpc['name'])
 
-#    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
-#    @patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_get_one)
+# TODO: to verify
+#    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+#    @patch('ibmcloud_python_sdk.vpc.vpc.qw', common.fake_get_one)
 #    def test_get_vpc_with_id(self):
 #        """Test get_vpc with id as parameter."""
 #        response = self.vpc.get_vpc(self.fake_vpc['id'])
 #        self.assertEqual(response['id'], self.fake_vpc['id'])
 
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
-    @patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_get_one)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+    @patch('ibmcloud_python_sdk.vpc.vpc.qw', common.fake_get_one)
     def test_get_vpc_default_security_group(self):
         """Test get_vpc_default_security_group."""
         response = self.vpc.get_vpc_default_security_group(self.fake_vpc['id'])
         print(response)
         self.assertEqual(response['default_security_group']['name'], self.fake_vpc['default_security_group'])
 
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
-    @patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_get_one)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+    @patch('ibmcloud_python_sdk.vpc.vpc.qw', common.fake_get_one)
     def test_get_vpc_default_network_acl(self):
         """Test get_vpc_default_network_acl."""
         response = self.vpc.get_vpc_default_network_acl(self.fake_vpc['id'])
         self.assertEqual(response['default_network_acl']['name'], self.fake_vpc['default_network_acl'])
 
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
-    @patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_create_vpc)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+    @patch('ibmcloud_python_sdk.vpc.vpc.qw', vpc.fake_create_vpc)
     def test_create_vpc_working(self):
         """Test create_vpc should work."""
         response = self.vpc.create_vpc(name=self.fake_vpc['name'])
         self.assertEqual(response['name'], self.fake_vpc['name'])
  
-    @patch('ibmcloud_python_sdk.auth.get_token', tcfg.fake_auth)
-    @patch('ibmcloud_python_sdk.vpc.vpc.qw', tcfg.fake_create_vpc)
+    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+    @patch('ibmcloud_python_sdk.vpc.vpc.qw', vpc.fake_create_vpc)
     def test_create_vpc_not_working(self):
         """Test create_vpc should not work."""
         response = self.vpc.create_vpc(name=self.fake_vpc['name'])
         self.assertNotEqual(response['id'], self.fake_vpc['name'])
-        
+       
