772f745cd00130a52216df8b17e130d8c906d473
James Regis <james.regis@bara.ca>
2020-03-30 23:09:49 -0400

    [tests] more test on instances

--- tests/instance/test_instance.py
+++ tests/instance/test_instance.py
@@ -17,7 +17,7 @@ class VPCTestCase(unittest.TestCase):
         self.instance = Instance()
         self.fake_instance = {}
         self.fake_instance['name'] = 'sdk'
-        self.fake_instance['id'] = 'r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82'
+        self.fake_instance['id'] = '0737_b06fd819-66d6-4802-ab51-f23061d981dd'
         self.fake_instance['default_security_group'] = 'fruit-average-shaping-gone-denture-rumor'
         self.fake_instance['default_network_acl'] = 'unpainted-crucial-trimmer-perennial-zipfile-barcode'
 
@@ -30,12 +30,12 @@ class VPCTestCase(unittest.TestCase):
         response = self.instance.get_instances()
         self.assertNotEqual(len(response), 0)
 
-#    @patch('ibmcloud_python_sdk.vpc.instance.qw', common.fake_get_call)
-#    def test_get_instance_with_name(self):
-#        """Test get_instance with name as parameter."""
-#        response = self.instance.get_instance(self.fake_instance['name'])
-#        self.assertEqual(response['name'], self.fake_instance['name'])
-#
+    @patch('ibmcloud_python_sdk.vpc.instance.qw', common.fake_get_call)
+    def test_get_instance_with_name(self):
+        """Test get_instance with name as parameter."""
+        response = self.instance.get_instance(self.fake_instance['name'])
+        self.assertEqual(response['name'], self.fake_instance['name'])
+
 ## TODO: to verify
 ##    @patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
 ##    @patch('ibmcloud_python_sdk.instance.instance.qw', common.fake_get_one)
@@ -57,15 +57,23 @@ class VPCTestCase(unittest.TestCase):
 #        response = self.instance.get_instance_default_network_acl(self.fake_instance['id'])
 #        self.assertEqual(response['default_network_acl']['name'], self.fake_instance['default_network_acl'])
 #
-#    @patch('ibmcloud_python_sdk.vpc.instance.qw', instance.fake_create_instance)
-#    def test_create_instance_working(self):
-#        """Test create_instance should work."""
-#        response = self.instance.create_instance(name=self.fake_instance['name'])
-#        self.assertEqual(response['name'], self.fake_instance['name'])
-# 
-#    @patch('ibmcloud_python_sdk.vpc.instance.qw', instance.fake_create_instance)
-#    def test_create_instance_not_working(self):
-#        """Test create_instance should not work."""
-#        response = self.instance.create_instance(name=self.fake_instance['name'])
-#        self.assertNotEqual(response['id'], self.fake_instance['name'])
-#       
+    @patch('ibmcloud_python_sdk.vpc.instance.qw', instance.fake_create_instance)
+    def test_create_instance_working(self):
+        """Test create_instance should work."""
+        response = self.instance.create_instance(name=self.fake_instance['name'], 
+                pni_subnet='subnet', 
+                image='image', 
+                profile='profile', 
+                zone='zone')
+        self.assertEqual(response['name'], self.fake_instance['name'])
+ 
+    @patch('ibmcloud_python_sdk.vpc.instance.qw', instance.fake_create_instance)
+    def test_create_instance_not_working(self):
+        """Test create_instance should not work."""
+        response = self.instance.create_instance(name=self.fake_instance['name'],
+                 pni_subnet='subnet', 
+                image='image', 
+                profile='profile', 
+                zone='zone')
+        self.assertNotEqual(response['id'], self.fake_instance['name'])
+       
