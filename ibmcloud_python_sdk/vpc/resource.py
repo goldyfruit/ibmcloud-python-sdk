c5a452d60f1cdc52fb1485cfacbcf4d778047fbe
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-01 15:51:14 -0400

    [resource] Add get_*_by_* methods.
    This allows to retrieve a specific resource group by ID or by name.

--- ibmcloud_python_sdk/vpc/resource.py
+++ ibmcloud_python_sdk/vpc/resource.py
@@ -5,6 +5,7 @@ from . import config as ic
 from ibmcloud_python_sdk.config import params
 from ibmcloud_python_sdk.auth import get_headers as headers
 from ibmcloud_python_sdk.utils.common import query_wrapper as qw
+from ibmcloud_python_sdk.utils.common import resource_not_found
 
 
 class Resource():
@@ -24,7 +25,66 @@ class Resource():
             return qw("rg", "GET", path, headers())["data"]
 
         except Exception as error:
-            print(f"Error fetching resource groups. {error}")
+
+    def get_resource_group(self, group):
+        """
+        Retrieve specific resource group by name or by ID
+        :param group: Resource group name or ID
+        """
+        by_name = self.get_resource_group_by_name(group)
+        if "errors" in by_name:
+            for key_name in by_name["errors"]:
+                if key_name["code"] == "not_found":
+                    by_id = self.get_resource_group_by_id(group)
+                    if "errors" in by_id:
+                        return by_id
+                    return by_id
+                else:
+                    return by_name
+        else:
+            return by_name
+
+    def get_resource_group_by_id(self, id):
+        """
+        Retrieve specific resource group by ID
+        :param id: Resource group ID
+        """
+        try:
+            # Connect to api endpoint for resource_groups
+            path = ("/v2/resource_groups/{}").format(id)
+
+            # Return data
+            return qw("rg", "GET", path, headers())["data"]
+
+        except Exception as error:
+            print("Error fetching resource group with ID {}. {}").format(
+                id, error)
+            raise
+
+    def get_resource_group_by_name(self, name):
+        """
+        Retrieve specific resource group by name
+        :param name: Resource group name
+        """
+        try:
+            # Connect to api endpoint for resource_groups
+            path = ("/v2/resource_groups")
+
+            # Retrieve resources data
+            data = qw("rg", "GET", path, headers())["data"]
+
+            # Loop over resources until filter match
+            for resource in data['resources']:
+                if resource["name"] == name:
+                    # Return data
+                    return resource
+
+            # Return error if no resource is found
+            return resource_not_found()
+
+        except Exception as error:
+            print("Error fetching resource group with name {}. {}").format(
+                name, error)
             raise
 
     # Get resource groups by account
