f5c32e62efcd3273ee289ebac6d584d6c4900335
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-31 23:47:50 -0400

    [examples] Update vpc examples

--- examples/vpc.py
+++ examples/vpc.py
@@ -7,7 +7,7 @@ vpc = ic.Vpc()
 vpc.get_vpcs()
 
 # Retrieve specific VPC (generic)
-vpc.get_vpc("advisory")
+vpc.get_vpc("ibmcloud-vpc-baby")
 
 # Retrieve specific VPC by ID
 vpc.get_vpc_by_id("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
@@ -16,11 +16,8 @@ vpc.get_vpc_by_id("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
 vpc.get_vpc_by_name("advisory")
 
 # Create VPC
-vpc.create_vpc(name="my-first-vpc",
+vpc.create_vpc(name="ibmcloud-vpc-baby",
                resource_group="f328f2cdec6d4b4da2844c214dec9d39")
 
-# Delete VPC by ID
-vpc.delete_vpc_by_id("r006-ea930372-2abd-4aa1-bf8c-3db3ac8cb765")
-
-# Delete VPC by name
-vpc.delete_vpc_by_name("my-first-vpc")
+# Delete VPC
+vpc.delete_vpc("ibmcloud-vpc-baby")
