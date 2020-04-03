98a9a0bb0e263c2aa5e6ce0824b3fd8d30ef3c6b
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-02 22:33:35 -0400

    [example] Update FIP example

--- examples/fip.py
+++ examples/fip.py
@@ -18,16 +18,10 @@ fip.get_floating_ip_by_name("ibmcloud-fip-baby")
 # Retrieve specific floating IP by address
 fip.get_floating_ip_by_address("128.128.129.129")
 
-# Create floating IP
+# Reserve floating IP
 fip.reserve_floating_ip(target="0737-968fd5b4-6548-44db-acf0-6ebd6d66a301",
-                        name="vm001", zone="us-south-3",
+                        name="vm001",
                         resource_group="f328f2cdec6d4b4da2844c214dec9d39")
 
-# Delete floating IP by ID
-fip.delete_floating_ip_by_id("0737-968fd5b4-6548-44db-acf0-6ebd6d66a301")
-
-# Delete floating IP by name
-fip.delete_floating_ip_by_name("ibmcloud-fip-baby")
-
-# Delete floating IP by using generic delete method
-fip.delete_floating_ip("ibmcloud-fip-baby")
+# Release floating IP
+fip.reserve_floating_ip("ibmcloud-fip-baby")
