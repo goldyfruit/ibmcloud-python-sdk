45d6a15af596ac6a790e20db2fdca3f32dca3c83
James Regis (jregis) <james.regis@bara.ca>
2020-04-02 23:45:30 -0400

    [dns] Change in dns

--- ibmcloud_python_sdk/dns/dns.py
+++ ibmcloud_python_sdk/dns/dns.py
@@ -2,7 +2,9 @@ import json
 from ibmcloud_python_sdk.config import params
 from ibmcloud_python_sdk.auth import get_headers as headers
 from ibmcloud_python_sdk.utils.common import query_wrapper as qw
+from ibmcloud_python_sdk.utils.common import resource_not_found
 
+from ibmcloud_python_sdk.utils.common import check_args
 
 class Dns():
 
@@ -76,6 +78,13 @@ class Dns():
     def get_resource_instance_by_name(self, name):
         resource_instances = self.get_resource_instances()
 
+        if len(resource_instances) == 0:
+            return ({"errors": [{"code": "not_found",
+                "msg": "No resource instance suitable for DNS operations found."}]})
+        
+        if "errors" in resource_instances:
+            return resource_instances
+
         for resource_instance in resource_instances:
             if resource_instance["name"] == name and \
                 resource_instance["resource_plan_id"] == self.resource_plan_id:
@@ -86,40 +95,83 @@ class Dns():
                     })
 
 
+    # Get specific dns zone id
+    def get_dns_zone_info(self, **kwargs):
+        # Required parameters
+        required_args = ['name', 'resource_instance']
+        #check_args(required_args, **kwargs)
+
+        # Set default value is not required paramaters are not defined
+        args = {
+            'name': kwargs.get('name'),
+            'resource_instance':  kwargs.get('resource_instance'),
+        }
+
+        # Construct payload
+        try:
+            resource_instance_guid = self.get_resource_instance(args['resource_instance'])["guid"]
+        except Exception as error:
+            print(f"Unable to find resource instance : {args['resource_instance']}.")
+            raise
+
+        try:
+            # Connect to api endpoint for vpcs
+            path = ("/v1/instances/{}/dnszones").format(
+                resouce_instance_guid)
+
+            dns_zones = qw("dns", "GET", path, headers(),
+                    json.dumps(payload))["data"]
+
+            if "error" in dns_zones or len(dns_zones) == 0:
+                return ({"error": [{"status_code": 404,
+                    "msg": "No dns zone found."}]})
+            
+            for dns_zone in dns_zones["data"]:
+                if dns_zone["name"] == name:
+                    return dns_zone
+                else:
+                    return ({"error": [{"status_code": 404, 
+                        "msg": "No dns zone found."}]})
+            # Return data
+
+        except Exception as error:
+            print(f"Error creating dns zone. {error}")
+            raise
+
 
     # Create DNS zone
     def create_zone(self, **kwargs):
         # Required parameters
         required_args = set(["name", "resource_instance"])
-        if not required_args.issubset(set(kwargs.keys())):
-            raise KeyError(
-                f'Required param is missing. Required: {required_args}'
-            )
+        check_args(required_args, **kwargs)
 
         # Set default value is not required paramaters are not defined
         args = {
             'name': kwargs.get('name'),
             'description': kwargs.get('description'),
             'label': kwargs.get('label'),
+            'resource_instance':  kwargs.get('resource_instance'),
         }
 
         # Construct payload
         payload = {}
 
+        resource_instance_guid = self.get_resource_instance(resource_instance)["guid"]
         for key, value in args.items():
              payload[key] = value
-            
+        
+                     
         try:
             # Connect to api endpoint for vpcs
-            path = ("/v1/instances?version={}&generation={}").format(
-                self.cfg["version"], self.cfg["generation"])
+            path = ("/v1/instances/{}/dnszones").format(
+                resouce_instance_guid)
 
             # Return data
             return qw("dns", "POST", path, headers(),
                     json.dumps(payload))["data"]
 
         except Exception as error:
-            print(f"Error creating instance. {error}")
+            print(f"Error creating dns zone. {error}")
             raise
 
 
