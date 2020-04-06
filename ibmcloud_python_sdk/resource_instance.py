120cee086bbbe4d2738a75b1fce4579640359fba
James Regis (jregis) <james.regis@bara.ca>
2020-04-06 15:45:11 -0400

    [resource_instance] Fix some bugs

--- ibmcloud_python_sdk/resource_instance.py
+++ ibmcloud_python_sdk/resource_instance.py
@@ -1,49 +1,84 @@
 import json
+import re
 from ibmcloud_python_sdk.config import params
 from ibmcloud_python_sdk.auth import get_headers as headers
 from ibmcloud_python_sdk.utils.common import query_wrapper as qw
 from ibmcloud_python_sdk.utils.common import resource_not_found
 
+from ibmcloud_python_sdk import resource_group
 from ibmcloud_python_sdk.utils.common import check_args
 
 class ResourceInstance():
 
     def __init__(self):
         self.cfg = params()
+        self.rg = resource_group.Resource()
         # self.resource_group_id = "aef66560191746fe804b9a66874f62b1"
-        self.resource_plan_id = "dc1460a6-37bd-4e2b-8180-d0f86ff39baa"
+        # self.resource_plan_id = "dc1460a6-37bd-4e2b-8180-d0f86ff39baa"
+        self.resource_plan_dict = {"dns": "dc1460a6-37bd-4e2b-8180-d0f86ff39baa",
+                }
 
     def create_resource_instance(self, **kwargs):
         """Create a resource instance
 
         :param: name: required.
-        :param: resource_group_id: required.
-        :param: resource_plan_id: required.
-        :param: target: required.
+        :param: resource_group: Optional. The default resource group will be 
+            used resource_group is None
+        :param: resource_plan: Optional. The plan will be the DNS plan by default
+        :param: target: optional. If not set the default value will be 
+            bluemix-global
         """
         # Required parameters
-        required_args = ['name', 'resource_group_id', 'resource_plan_id', 'target']
+        required_args = ['name', 'resource_group', 'resource_plan', 'target']
         check_args(required_args, **kwargs)
  
         # Set default value if required paramaters are not defined
         args = {
             'name': kwargs.get('name'),
-            'resource_group': kwargs.get('resource_group_id'),
-            'resource_plan_id': kwargs.get('resource_plan_id'),
+            'resource_group': kwargs.get('resource_group'),
             'target': kwargs.get('target'),
         }
   
+        resource_plan: kwargs.get('resource_plan')
         payload = {}
 
         # Construct payload
-        for key, value in args.items():
-            payload[key] = value
+        payload["name"] = args["name"]
+
+        payload["resource_plan_id"] = self.get_resource_plan_id(
+                kwargs.get('resource_plan'))
+
+        if args["target"] == None:
+            payload["target"] = "bluemix-global"
+        else:
+            payload["target"] = args["target"]
+      
+        if args["resource_group"] == None:
+            payload["resource_group"] = \
+                    self.rg.get_default_resource_group()["id"]
+        else:
+            payload["resource_group"] = args["resource_group"]
+
+#        for key, value in args.items():
+#            if value is None:
+#                if key == "resource_group":
+#                    payload["resource_group"] = \
+#                            self.rg.get_default_resource_group()["id"]
+#                if key == "target":
+#                    payload["target"] = "bluemix-global"
+#            if value is not None or value == '':
+#                if key == "resource_group":
+#                    rg_info = self.rg.get_resource_group(
+#                            args["resource_group"])
+#                    if "errors" in rg_info:
+#                        return rg_info
+#                    payload["resource_group"] = rg_info["id"]
+#            payload[key] = value
 
         try:
             # Connect to api endpoint for resource instances
             path = ("/v2/resource_instances")
 
-            print(json.dumps(payload))
             return qw("rg", "POST", path, headers(),
                     json.dumps(payload))["data"]
         except Exception as error:
@@ -51,12 +86,33 @@ class ResourceInstance():
             raise
        
 
+    def get_resource_plan_id(self, resource_plan):
+        """Return resource_plan_id based on the input
+
+        :param resource_plan: Required. Resource plan Name or ID
+        """
+        # if ressource_plan is an id, return the id
+        rp_pattern = ('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+        rp_compile = re.compile(rp_pattern)
+        
+        if resource_plan is None or resource_plan == '':
+             return self.resource_plan_dict['dns']
+
+        # if resource_plan match the regexp, return ressource_plan
+        if (bool(rp_compile.search(resource_plan))):
+            return resource_plan
+        elif resource_plan in self.resource_plan_dict :            
+            return self.resource_plan_dict[resource_plan]
+        # return dns resource instance id by default
+        else:
+            return self.resource_plan_dict['dns']
+
     def get_resource_instances(self, resource_plan_id=None):
         """Retrieve all dns resource instances
         """
         # set resource_plan default to DNS 
         if resource_plan_id == None:
-            resource_plan_id = self.resource_plan_id
+            resource_plan_id = self.resource_plan_dict['dns']
         result = []
         try:
             # Connect to api endpoint for resource instances
@@ -108,11 +164,11 @@ class ResourceInstance():
 
     # Get specific resource instance by name
     def get_resource_instance_by_name(self, name):
-        resource_instances = self.get_resource_instances()
+        resource_instances = self.get_resource_instances(resource_plan_id=None)
         
         if len(resource_instances) == 0:
             return ({"errors": [{"code": "not_found",
-                "message": "No resource instance suitable for DNS operations found."}]})
+                "message": "No resource instances found."}]})
         
         if "errors" in resource_instances:
             return resource_instances
@@ -120,11 +176,11 @@ class ResourceInstance():
         for resource_instance in resource_instances:
             if resource_instance["name"] == name:
                     return resource_instance
-            else:
-                return ({"errors": [{"code": "not_found", 
-                    "message": "No resource instance suitable for DNS operations found."}]
-                    })
-    def delete_reource_instance(self, **kwargs):
+        return ({"errors": [{"code": "not_found", 
+            "message": "No resource instance found."}]
+            })
+
+    def delete_resource_instance(self, **kwargs):
         """Delete a resource instance
 
         :param: id: required. The resource instance id
