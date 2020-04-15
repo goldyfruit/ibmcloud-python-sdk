ac3e9ed88dab53351e3514664bb04b29a0e1e96e
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-15 18:11:22 -0400

    [em] Add account and enterprise classes

---
+++ ibmcloud_python_sdk/em/account.py
@@ -0,0 +1,24 @@
+from ibmcloud_python_sdk.config import params
+from ibmcloud_python_sdk.auth import get_headers as headers
+from ibmcloud_python_sdk.utils.common import query_wrapper as qw
+
+
+class Account():
+
+    def __init__(self):
+        self.cfg = params()
+
+    def get_accounts(self):
+        """
+        Retrieve account list
+        """
+        try:
+            # Connect to api endpoint for accounts
+            path = ("/v1/accounts")
+
+            # Return data
+            return qw("em", "GET", path, headers())["data"]
+
+        except Exception as error:
+            print("Error fetching accounts. {}".format(error))
+            raise
---
+++ ibmcloud_python_sdk/em/enterprise.py
@@ -0,0 +1,24 @@
+from ibmcloud_python_sdk.config import params
+from ibmcloud_python_sdk.auth import get_headers as headers
+from ibmcloud_python_sdk.utils.common import query_wrapper as qw
+
+
+class Enterprise():
+
+    def __init__(self):
+        self.cfg = params()
+
+    def get_enterprises(self):
+        """
+        Retrieve enterprise list
+        """
+        try:
+            # Connect to api endpoint for enterprises
+            path = ("/v1/enterprises")
+
+            # Return data
+            return qw("em", "GET", path, headers())["data"]
+
+        except Exception as error:
+            print("Error fetching enterprises. {}".format(error))
+            raise
