3d6d63fb6414a314380b7ad6c82b405dbf6e031f
James Regis <james.regis@ibm.com>
2020-09-03 16:59:59 -0400

    [tests/resource_group/test_resource_group.py] flake8 compliance change

--- tests/resource_group/test_resource_group.py
+++ tests/resource_group/test_resource_group.py
@@ -2,23 +2,27 @@ import unittest
 
 from mock import patch
 
-import ibmcloud_python_sdk.config
+# import ibmcloud_python_sdk.config
 from ibmcloud_python_sdk.resource.resource_group import ResourceGroup
 
 import tests.common as common
+
 
 class ResourceTestCase(unittest.TestCase):
     """Test case for the client methods."""
 
     def setUp(self):
-        self.patcher = patch('ibmcloud_python_sdk.auth.get_token', common.fake_auth)
+        self.patcher = patch('ibmcloud_python_sdk.auth.get_token',
+                             common.fake_auth)
         self.patcher.start()
         self.resource = ResourceGroup()
         self.fake_resource = {}
         self.fake_resource['name'] = 'sdk'
         self.fake_resource['id'] = '0737_b06fd819-66d6-4802-ab51-f23061d981dd'
-        self.fake_resource['default_security_group'] = 'fruit-average-shaping-gone-denture-rumor'
-        self.fake_resource['default_network_acl'] = 'unpainted-crucial-trimmer-perennial-zipfile-barcode'
+        self.fake_resource['default_security_group'] = 'fruit-average-shaping\
+                                                        -gone-denture-rumor'
+        self.fake_resource['default_network_acl'] = 'unpainted-crucial-trimmer-\
+                                                    perennial-zipfile-barcode'
 
     def tearDown(self):
         self.patcher.stop()
@@ -32,6 +36,8 @@ class ResourceTestCase(unittest.TestCase):
     @patch('ibmcloud_python_sdk.resource_group.qw', common.fake_get_call)
     def test_get_resource_with_name(self):
         """Test get_resource with name as parameter."""
-        response = self.resource.get_resource_groups_by_account(self.fake_resource['name'])
+        response = self.resource.get_resource_groups_by_account(
+            self.fake_resource['name'])
         print(response)
-        self.assertEqual(response['resource_groups'][0]['name'], self.fake_resource['name'])
+        self.assertEqual(response['resource_groups'][0]['name'],
+                         self.fake_resource['name'])
