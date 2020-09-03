3e2c950cddebf08d9ce29f5aea9b151522f61b4a
James Regis <james.regis@ibm.com>
2020-09-03 17:00:15 -0400

    [tests/vpc/custom.py] flake8 compliance change

--- tests/vpc/custom.py
+++ tests/vpc/custom.py
@@ -1,16 +1,12 @@
 import os.path
-import requests
-import re
 import json
 
-from urllib.parse import urlparse
 
 def fake_get_vpc(self, vpc):
     """
     Return a false header required by authentication process
     """
     result = {}
-    folder = 'vpcs'
     resource_file = os.path.normpath('tests/resources/vpcs/vpcs.json')
     # Must return a file-like object
     try:
@@ -18,9 +14,6 @@ def fake_get_vpc(self, vpc):
         result = json.load(json_file)
         return(result)
     except IOError:
-        print("in da house")
         result["data"] = {"error": "not found"}
         return result
         raise
-
-
