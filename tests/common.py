36baff349c0b0e5569debd36f26f3de82828bd9d
James Regis <james.regis@ibm.com>
2020-09-03 16:57:05 -0400

    [tests/common.py] flaks8 compliance change

--- tests/common.py
+++ tests/common.py
@@ -1,9 +1,7 @@
 import os.path
-import requests
-import re
 import json
 
-from urllib.parse import urlparse
+# from urllib.parse import urlparse
 
 
 def set_folder_var(path):
@@ -31,14 +29,16 @@ def set_folder_var(path):
 
     return folder
 
+
 def fake_auth(auth_url, key):
     """
     Return a false header required by authentication process
     """
-    return {'Content-Type': 'application/json', \
-            'Accept': 'application/json', 
+    return {'Content-Type': 'application/json',
+            'Accept': 'application/json',
             'Authorization': 'Bearer eyJraWQiOiIyMDIwMDMyNjE4MjgiLCJhbGciOiJ\
                     SUzI1NiJ9.e'}
+
 
 def fake_get_call(service, verb, path, headers):
     """
@@ -48,8 +48,10 @@ def fake_get_call(service, verb, path, headers):
     result["data"] = {}
     folder = set_folder_var(path)
     result["data"][folder] = []
-    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
-    #print(resource_file)
+    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(
+        folder,
+        folder
+    )
     # Must return a file-like object
     try:
         json_file = open(resource_file, mode='rb')
@@ -58,16 +60,19 @@ def fake_get_call(service, verb, path, headers):
     except IOError:
         print("in da house")
         return result["data"][folder].append({"error": "not found"})
-        raise 
+        raise
+
 
 def fake_get_one(service, verb, path, headers):
     """
     This function will replace the original API.
     """
     result = {}
-    
     folder = set_folder_var(path)
-    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
+    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(
+        folder,
+        folder
+    )
     # Must return a file-like object
     try:
         json_file = open(resource_file, mode='rb')
@@ -83,32 +88,30 @@ def fake_create(service, verb, path, headers, payload):
     Test
     """
     data = {}
-    folder = set_folder_var(path)
-#    path_pattern = '\/v1\/'+folder+'\?version=[0-9]{4}-[0-9]{2}-[0-9]{2}&generation=[0-9]'
-#    vpc_acl_or_sg_pattern = '\/v1\/vpcs\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/default_security_group\?version=[0-9]{4}-[0-9]{2}-[0-9]{2}\&generation=[0-9]{1}'
-#    contain_path = re.compile(path_pattern)
-#    contain_acl_or_sg_path = re.compile(vpc_acl_or_sg_pattern)
-    #print(path_pattern)
-    #print(path)
-#    print(bool(contain_path.search(path)))
-#    print(bool(contain_acl_or_sg_path.search(path)))
-    if service == "iaas" and verb == "POST" : #and\
-#            (bool(contain_path.search(path)) is True or bool(contain_acl_or_sg_path(path)) is True):
-        data = { "id": "r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82", \
-                "crn": "crn:v1:bluemix:public:is:us-south:a/2d171b8a90e246fd9ffe0e5e8c191c9e:\
-                :instance:r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82", \
-                "href": "https://us-south.iaas.cloud.ibm.com/v1/instances/r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82",\
-                "name": "sdk", \
-                "status": "available"}
+    # folder = set_folder_var(path)
+    if service == "iaas" and verb == "POST":
+        data = {"id": "r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82",
+                "crn": "crn:v1:bluemix:public:is:us-south:a/2d171b8a90\
+                 e246fd9ffe0e5e8c191c9e::instance:r006-74ff2772-9f3a-4263\
+                -bcaa-12fcffa3ed82",
+                "href": "https://us-south.iaas.cloud.ibm.com/v1/instances\
+                        /r006-74ff2772-9f3a-4263-bcaa-12fcffa3ed82",
+                "name": "sdk",
+                "status": "available"
+                }
         return({"status_code": 200, "data": data})
     else:
         data = {"id": "", "name": "", "status": "error !"}
         return({"status_code": 500, "data": data})
 
+
 def query_specified_object(path):
-    #result = {}
+    # result = {}
     folder = set_folder_var(path)
-    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(folder, folder)
+    resource_file = os.path.normpath('tests/resources/{}/{}.json').format(
+        folder,
+        folder
+    )
     # Must return a file-like object
     try:
         json_file = open(resource_file, mode='rb')
@@ -126,33 +129,34 @@ def fake_get_vpc(fake, data):
     result = query_specified_object('vpc')
     return(result)
 
+
 def fake_get_resource_group(fake, data):
     """
     This function will replace the original API.
     """
     result = query_specified_object('resource_group')
     return(result)
- 
+
+
 def fake_get_image(fake, data):
     """
     This function will replace the original API.
     """
     result = query_specified_object('image')
     return(result)
- 
+
+
 def fake_get_vpn(fake, data):
     """
     This function will replace the original API.
     """
     result = query_specified_object('vpn')
     return(result)
- 
+
+
 def fake_get_floating_ip(fake, data):
     """
     This function will replace the original API.
     """
     result = query_specified_object('floating_ips')
     return(result)
- 
-
-
