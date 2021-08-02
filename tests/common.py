f980f7cba31c6e555f7b96fd970d5c73339a25ae
Gaëtan Trellu <gaetan.trellu@gmail.com>
2021-08-02 18:53:01 -0400

    [test/common] Add functions documentation

--- tests/common.py
+++ tests/common.py
@@ -5,27 +5,37 @@ from types import SimpleNamespace
 from tests.constants import FOLDERS, UUID_REGEXP
 
 
-def get_headers(url, key):
+def get_headers(arg1=None, arg2=None):
+    """This function adds hearders that are required by
+    ibmcloud_python_sdk.auth.get_token method. arg1 and arg2 are not used.
+    """
     return {'Content-Type': 'application/json'}
 
-def get_one(path):
+def get_one(path, index=0):
+    """This function returns only one item of the list. The
+    item to return could be selected by using the index variable.
+    """
     for key, value in FOLDERS.items():
         if path == key:
             data_file = f'{os.path.dirname(__file__)}/{value}/{key}.json'
             with open(data_file, 'r') as json_file:
                 try:
-                    return {"data": json.load(json_file)[key][0]}
+                    return {"data": json.load(json_file)[key][index]}
                 except json.JSONDecodeError as err:
                     return err
 
     return {'error': 'unable to read data'}
 
 def get_all(path):
+    """This function returns all the items from the JSON file except if a
+    UUID is detected in the URL path then only one item will be returned.
+    """
     is_uuid = re.findall(UUID_REGEXP, path)
     uri = None
 
     if is_uuid:
         #/v1/subnets/...
+        #/v1/subnets/.../...
         uri = re.match(r'/v1/([^/]+)', path).group(1)
     else:
         #/v1/subnets?...
@@ -44,24 +54,32 @@ def get_all(path):
 
     return {'error': 'unable to read data'}
 
-def qw(conn_type, method, path, headers=None, payload=None):
+def qw(arg1, arg2, path, headers=None, payload=None):
+    """This function is used to mock the query_wrapper function from
+    utils/common. It returns information collected from get_all()
+    function.
+    """
     return get_all(path)
 
 def qw_not_found(arg1, arg2, path=None, headers=None, payload=None):
+    """This function is used when a not found resource should be returned.
+    According arg2 argument, if "not_found" is detected as a string then
+    payload is returned directly or as part of "data" dictionnary.
+    """
+    payload = {'errors': [{
+                  'code': 'not_found',
+                  'message': 'Resource not found'
+                  }]
+              }
     if arg2 == 'not_found':
-        return  {'errors': [{
-                    'code': 'not_found',
-                    'message': 'Resource not found'
-                    }]
-                }
-    return  {'data': {
-                'errors': [{
-                    'code': 'not_found',
-                    'message': 'Resource not found'
-                }]
-            }}
+        return payload
+    return  {'data': payload}
 
-def qw_api_error(conn_type, method, path, headers=None, payload=None):
+def qw_api_error(arg1, arg2, path, headers=None, payload=None):
+    """This function is used when an unpredicatble error shoudl be returned
+    as if something went wrong with the API call. None of the arguments will
+    be used.
+    """
     return  {'data': {
                 'errors': [{
                     'code': 'unpredictable_error',
@@ -69,15 +87,29 @@ def qw_api_error(conn_type, method, path, headers=None, payload=None):
                 }]
             }}
 
-def qw_exception(conn_type, method, path, headers=None, payload=None):
+def qw_exception(arg1, arg2, path, headers=None, payload=None):
+    """This function is used when an exception should be raised. None of the
+    arguments will be used.
+    """
     raise Exception
 
 def qw_delete_code_204(arg1, arg2, path, headers=None, payload=None):
+    """This function is used when a code 204 is required during a resource
+    deleting. Using the SimpleNamespace() will help to create an object that
+    will contains status as part of the dict. None of the arguments will be
+    used. SimpleNamespace() explaination: https://bit.ly/3ylloVs
+    """
     response = SimpleNamespace()
     response.status = 204
     return {'response': response}
 
 def qw_delete_code_400(arg1, arg2, path, headers=None, payload=None):
+    """This function is used when a code 400 is required during a resource
+    deleting to simulate an issue. Using the SimpleNamespace() will help to
+    create an object that will contains status as part of the dict. None of
+    the arguments will be used. SimpleNamespace() explaination:
+    https://bit.ly/3ylloVs
+    """
     response = SimpleNamespace()
     response.status = 400
     return {'response': response, 'data': 'bad_request'}
